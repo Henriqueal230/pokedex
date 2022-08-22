1565ba82a8ad24e4fc206a3bc636bb1db008a137
henrique <henrique.almeida231@hotmail.com>
2022-08-22 19:46:52 -0300

    debug

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
 import './App.css';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
 import { Cardpokemon } from './components/card-pokemon/cardpokemon';
 import { Header } from './components/header/header';
 
@@ -19,19 +14,7 @@ function Home(){
 export default function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/pokedex" element={<Home />} />
-          <Route
-          path="*"
-          element={
-            <main style={{ padding: "1rem" }}>
-              <p>There's nothing here!</p>
-            </main>
-      }
-    />
-        </Routes>
-      </BrowserRouter>
+      <Home />
     </div>
   );
 }
